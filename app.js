11b9dd83c7a59feeae21c46d3d9be2c4803bd126
Mitch Lembke <mitchell.lembke@gmail.com>
2018-03-21 10:02:59 -0600

    built again

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
 const express = require('express')
 const app = express()
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 3000
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
 
 app.disable('x-powered-by')
-app.use(bodyParser.json())
 app.use(morgan('dev'))
+app.use(bodyParser.json())
 
-app.get('/', (req, res) => {
-  res.send('awesome!')
+app.get('/', (req, res) =>{
+  res.send('awesome! it works')
 })
 
 app.use((err, req, res, next) => {
   const status = err.status || 500
-  res.status(status).json({ error: err })
+  res.status(status).json({error: err})
 })
 
 app.use((req, res, next) => {
-  res.status(404).json({error: {message: 'Not found.'}})
+  res.status(404).json({error: {message: 'Not found'}})
 })
 
 const listener = (err) => {
-  if(err) console.log(err)
+  if (err) console.log(err)
   console.log(`App listening on port ${PORT}`)
 }
 
